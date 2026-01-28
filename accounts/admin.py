27c677b7dbfaa70ad9aefabf93da357df7e9ddc7
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-01-28 10:13:03 +0600

    Register CustomUser in Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("mobile", "email", "is_staff", "is_active")
+    search_fields = ("mobile", "email")
+    ordering = ("-created_at",)
