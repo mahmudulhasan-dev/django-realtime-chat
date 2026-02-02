e541ded188659fb34b62635fcb86f1a2b4cfabbd
Mahmudul Hasan <mclean.hasan@gmail.com>
2026-02-02 23:15:16 +0600

    Add ChatConsumer with connect and disconnect handling

---
+++ chat/consumers.py
@@ -0,0 +1,24 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth import get_user_model
+from .models import Room, Message
+
+User = get_user_model()
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_group_name = f"chat_{self.room_id}"
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
